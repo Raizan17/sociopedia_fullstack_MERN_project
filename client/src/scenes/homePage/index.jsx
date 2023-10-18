86fb14ab17a89f7ac7ef3481ec391bab3395e0ce
Raizan17 <raizan.lee17@gmail.com>
2023-10-17 19:46:53 -0600

    Completed Navbar
    Finished Making Navbar, Made use of FlexBetween component. Made use of MUI code, for Box Component and made use of useSelector to get values from states.

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -1,7 +1,14 @@
+import {Box} from "@mui/material";
+import Navbar  from "scenes/navbar";
+
 // difference between index.jsx and index.js is jsx files have react components in them so its just a syntactic sugar way to know which file has what content
 
 const HomePage = () => {
-  return (<div>homepage</div>);
+  return (
+  <Box>
+    <Navbar />
+  </Box>
+  );
 };
 
 export default HomePage;
