a52dd01658c90bb93247591244dbf834f9013b8a
Raizan17 <raizan.lee17@gmail.com>
2023-10-17 18:38:51 -0600

    Redux and Toolkit Installation and Setup
    Installed and setup redux, and redux toolkit to make it easier to manage states.

---
+++ client/src/scenes/homePage/index.jsx
@@ -0,0 +1,7 @@
+// difference between index.jsx and index.js is jsx files have react components in them so its just a syntactic sugar way to know which file has what content
+
+const HomePage = () => {
+  return (<div>homepage</div>);
+};
+
+export default HomePage;
